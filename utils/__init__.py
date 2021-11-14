6e7a256eebe0d19a0123a6e5424616716851470d
matias-jpg <63820402+matias-jpg@users.noreply.github.com>
2021-11-14 15:37:28 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,5 +7,5 @@ from utils.chess import plot_lines, plot_corners
 from utils.chess import get_intersections, mask_contour
 from utils.chess import ChessBoard
 
-from utils.flappy import Bird, Pipe, renderFrame
-from utils.shrek import Shrek
+#from utils.flappy import Bird, Pipe, renderFrame
+#from utils.shrek import Shrek
